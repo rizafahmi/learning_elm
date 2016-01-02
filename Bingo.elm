f1bb07187e93d53310da02cb14d8a2f2bfaf3837
Riza Fahmi <rizafahmi@gmail.com>
2016-01-02 18:12:07 +0700

    Finish generating Html
    https://online.pragmaticstudio.com/courses/elm/modules/6

--- Bingo.elm
+++ Bingo.elm
@@ -1,18 +1,32 @@
 module Bingo where
 
-import Html
-import String
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+import String exposing (toUpper, repeat, trimRight)
 
 title message times =
   message ++ " "
-  |> String.toUpper
-  |> String.repeat times
-  |> String.trimRight
-  |> Html.text
+  |> toUpper
+  |> repeat times
+  |> trimRight
+  |> text
 
 greet name colorPrimary colorSecondary colorTri =
   name ++ "'s favorites are: " ++ colorPrimary ++ " " ++ colorSecondary ++ " " ++ colorTri
 
+pageHeader =
+  h1 [] [ title "Bingo!" 3 ]
+
+pageFooter =
+  footer [] [
+            a [ href "http://citizenlab.co", target "_blank" ]
+              [ text "CitizenLab" ]
+           ]
+view =
+  div [ id "container" ] [ pageHeader, pageFooter ]
+
 
 main =
-  greet "Larry" "Blue" "Chocolate" "Lizard" |> Html.text
+  view
