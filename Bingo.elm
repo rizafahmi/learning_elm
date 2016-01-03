cfb2c32fa0a813461f32c7aef48daaa05350fa03
Riza Fahmi <rizafahmi@gmail.com>
2016-01-03 14:32:41 +0700

    Update Elm. Then finish updating the model
    https://online.pragmaticstudio.com/courses/elm/modules/11

--- Bingo.elm
+++ Bingo.elm
@@ -8,20 +8,32 @@ import String exposing (toUpper, repeat, trimRight)
 
 -- MODEL
 
-newEntry phrase point id =
+newEntry phrase points id =
   {
     phrase = phrase,
-    point = point,
+    points = points,
     wasSpoken = False,
     id = id
   }
 
 initialModel =
   { entries = [
-     newEntry "Doing Agile" 100 2,
+     newEntry "Doing Agile" 400 2,
      newEntry "Learn Asana" 200 1,
      newEntry "Procastinate" 350 3
     ]}
+
+-- UPDATE
+type Action
+  = NoOp
+  | Sort
+
+update action model =
+  case action of
+    NoOp ->
+      model
+    Sort ->
+      { model | entries = List.sortBy .points model.entries }
 
 -- VIEW
 
@@ -45,7 +57,7 @@ pageFooter =
 listItem entry =
   li [ ] [
         span [ class "phrase" ] [ text entry.phrase ],
-        span [ class "point" ] [ text ( toString entry.point ) ]
+        span [ class "point" ] [ text ( toString entry.points ) ]
        ]
 listEntries entries =
   ul [ ] ( List.map listItem entries )
@@ -56,4 +68,4 @@ view model =
 
 
 main =
-  view initialModel
+  view ( update Sort initialModel )
